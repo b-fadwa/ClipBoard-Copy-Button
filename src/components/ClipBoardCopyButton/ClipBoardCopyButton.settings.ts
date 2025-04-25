3749eeb4fdc7f0c0358b0bacd114ee24c80ac3d2
Fadwa BAYED <fadwa.bayed@4d.com>
2025-04-25 11:49:59 +0100

    copy action init

---
+++ src/components/ClipBoardCopyButton/ClipBoardCopyButton.settings.ts
@@ -0,0 +1,8 @@
+import { TSetting } from '@ws-ui/webform-editor';
+import { BASIC_SETTINGS, DEFAULT_SETTINGS, load } from '@ws-ui/webform-editor';
+
+const Settings: TSetting[] = [...DEFAULT_SETTINGS];
+
+export const BasicSettings: TSetting[] = [...load(BASIC_SETTINGS).filter('style.overflow')];
+
+export default Settings;
--- src/components/index.tsx
+++ src/components/index.tsx
@@ -1,5 +1,7 @@
 // imports
+import ClipBoardCopyButton from './ClipBoardCopyButton';
 
 export default {
   // components
+  ClipBoardCopyButton,
 };
